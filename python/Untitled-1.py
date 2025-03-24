42eb29ed016dca3a3097e90bb29f2d35307385b8
chawj <hanmy3541@gmail.com>
2025-03-24 12:46:14 +0900

    scond commit:   python/Untitled-1.py

--- python/Untitled-1.py
+++ python/Untitled-1.py
@@ -6,3 +6,25 @@ print("Hello World!")
 #자신이 백준 온라인 저지(BOJ)에서 맞은 문제의 수와 아이디를 그대로 출력하는 프로그램을 작성하시오.
 print("2\n")
 print("chj3172")
+#문제 3
+#고양이를 출력한다.
+print("\    /\ \n )  ( ') \n(  /  ) \n \(__)|")
+#문제 4 
+#강아지 출력
+print("|\\_/|")
+print("|q p|   /}")
+print('( 0 )"""\\')
+print('|"^"`    |')
+print("||_/=\\\\__|")
+#문제 5 
+#날짜/시간 출력
+import datetime
+print(datetime.datetime.today().strftime('%Y-%m-%d'))
+#문제6
+#새싹 출력
+print('         ,r\'\"7')
+print('r`-_   ,\'  ,/')
+print(' \\. \". L_r\'')
+print('   `~\\/')
+print('      |')
+print('      |')
