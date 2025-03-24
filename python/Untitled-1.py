3c20e33670638087c06474cde4ed3a5d4adf5f43
chawj <hanmy3541@gmail.com>
2025-03-24 10:45:04 +0900

    first commit

--- python/Untitled-1.py
+++ python/Untitled-1.py
@@ -1,2 +1,8 @@
-print("hellp")
-print("GOOD bye")
+#문제1
+#Hello World!를 출력하시오.
+print("Hello World!")
+
+#문제2
+#자신이 백준 온라인 저지(BOJ)에서 맞은 문제의 수와 아이디를 그대로 출력하는 프로그램을 작성하시오.
+print("2\n")
+print("chj3172")
